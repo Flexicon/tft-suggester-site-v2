207e3260098e0b536a6621c923596eb572675606
Mike Repec <mikerepec@gmail.com>
2023-06-20 21:25:27 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1 class="text-center mt-10 text-3xl font-bold">
+  SvelteKit + Tailwind + TFT = TFT Suggesting 🧞
+</h1>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
