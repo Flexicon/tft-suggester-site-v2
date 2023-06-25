293f943002d02109e0312da78c61b9419992a248
Mike Repec <mikerepec@gmail.com>
2023-06-25 11:50:15 +0200

    Style search input

--- src/lib/components/ChampionsSelector.svelte
+++ src/lib/components/ChampionsSelector.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Icon from '@iconify/svelte';
 	import { createEventDispatcher } from 'svelte';
 	import type { Champion } from '$lib/types';
 	import ChampionsGrid from './ChampionsGrid.svelte';
@@ -29,7 +30,12 @@
 	$: filteredChampions = filterChampions(champions, selectedNames, query.toLowerCase());
 </script>
 
-<input class="mb-5" type="text" name="query" bind:value={query} {placeholder} />
+<div class="form-control">
+	<input type="text" name="query" bind:value={query} {placeholder} />
+	<div class="icon">
+		<Icon icon="mdi:search" />
+	</div>
+</div>
 
 {#if showGrid}
 	<ChampionsGrid champions={filteredChampions} on:select={selectChampion} />
@@ -40,3 +46,22 @@
 		</p>
 	{/if}
 {/if}
+
+<style lang="postcss">
+	.form-control {
+		@apply relative;
+	}
+
+	.form-control input {
+		@apply pl-12 pr-4 py-3 mb-5 w-full rounded text-xl border shadow-inner;
+		@apply hover:border-zinc-300;
+	}
+
+	.form-control .icon {
+		@apply absolute text-zinc-300 text-4xl top-2 left-2;
+	}
+
+	input:focus + .icon {
+		@apply text-zinc-500;
+	}
+</style>
